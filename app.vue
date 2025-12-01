b8bcc195943e23edd4a2d688cee78e6a6b6f54cc
adam5096 <adamchou5096@gmail.com>
2025-12-01 19:25:02 +0800

    install and config vuetify-nuxt-module

--- app.vue
+++ app.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
+  <v-app>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <v-main>
+      <v-container>
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="8" md="6">
+            <v-card>
+              <v-card-title class="text-h5 text-center pa-4">
+                歡迎使用 Vuetify + Nuxt
+              </v-card-title>
+              <v-card-text class="text-center pa-4">
+                <p>Vuetify Nuxt 模組已成功安裝並配置！</p>
+                <v-btn color="primary" class="mt-4">
+                  開始使用
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,21 @@ export default defineNuxtConfig({
 
   modules: [
     '@nuxt/image',
-    '@nuxt/ui',
+    // '@nuxt/ui', // 已註解：需要 Nuxt 4.0.0，目前使用 Nuxt 3.20.1
     '@nuxt/test-utils',
-    '@nuxt/eslint'
-  ]
+    '@nuxt/eslint',
+    'vuetify-nuxt-module'
+  ],
+
+  // Vuetify 配置（可選，模組已提供預設配置）
+  vuetify: {
+    moduleOptions: {
+      /* 模組特定選項 */
+      // 如需自訂配置，請參考：https://nuxt.vuetifyjs.com/guide/
+    },
+    vuetifyOptions: {
+      /* Vuetify 選項 */
+      // 如需自訂 Vuetify 配置，請參考：https://nuxt.vuetifyjs.com/guide/
+    }
+  }
 })
